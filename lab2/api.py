dafaafd6ccf382518886d7f90745104c46a05f42
NNT3010 <nhuttruong3010@gmail.com>
2025-05-26 14:46:50 +0700

    cap nhat 2

--- lab2/api.py
+++ lab2/api.py
@@ -3,30 +3,27 @@ from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
 from cipher.playfair import PlayFairCipher
-from cipher.transposition import TranspositionCipher
-app = Flask(__name__)
-
-caesar_cipher = CaesarCipher()
-
-@app.route("/api/caesar/encrypt", methods=["POST"])
+from cipher.Transposition import TranspositionCipher
+app = Flask(__name__) 
+#Caesar
+caesar_cipher = CaesarCipher();
+@app.route('/api/caesar/encrypt', methods=['POST'])
 def caesar_encrypt():
-    data =  request.json
-    plain_text = data['plain_text']
-    key = int(data['key'])
-    encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
-    return jsonify({'encrypted_message' : encrypted_text})
+    data = request.get_json()
+    text = data['plain_text']
+    key = data['key']
+    return jsonify({'encrypted_text': caesar_cipher.encrypt_text(text, key)})
 
-@app.route("/api/caesar/decrypt", methods=["POST"])
+@app.route('/api/caesar/decrypt', methods=['POST'])
 def caesar_decrypt():
-    data =  request.json
-    cipher_text = data['cipher_text']
-    key = int(data['key'])
-    decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
-    return jsonify({'decrypted_message' : decrypted_text})
+    data = request.get_json()
+    text = data['cipher_text']
+    key = data['key']
+    return jsonify({'decrypted_text': caesar_cipher.decrypt_text(text, key)})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
 
+
+#Vigenere
 vigenere_cipher = VigenereCipher()
 @app.route('/api/vigenere/encrypt', methods=['POST'])
 def vigenere_encrypt():
@@ -42,8 +39,7 @@ def vigenere_decrypt():
     key = data['key']
     return jsonify({'decrypted_text': vigenere_cipher.decrypt_text(text, key)})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 #Railfence
 railfence_cipher = RailFenceCipher()
